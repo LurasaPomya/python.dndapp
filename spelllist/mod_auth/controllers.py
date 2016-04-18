a736ededbbb5696e0a6020bc40d6405c039b73ca
dectala <jemcgrew@gmail.com>
2016-04-18 15:32:25 -0400

    THere we go

--- spelllist/mod_auth/controllers.py
+++ spelllist/mod_auth/controllers.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, render_template, \
     flash, g, session, redirect, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 from spelllist import db
-from spelllist.mod_auth.forms import LoginForm
+from spelllist.mod_auth.forms import LoginForm, CreateUserForm
 from spelllist.mod_auth.models  import User
 
 mod_auth = Blueprint('auth', __name__, url_prefix='/auth')
@@ -22,11 +22,28 @@ def signin():
         if user and check_password_hash(user.password, form.password.data):
 
             session['user_id'] = user.id
-
             flash('Welcome %s' % user.username)
 
-            return redirect(url_for('auth.home'))
+            return redirect(url_for('spell_list'))
 
         flash('Wrong email or password', 'error-message')
 
     return render_template("auth/signin.html", form=form)
+
+# @mod_auth.route('/createuser/', methods=['GET', 'POST'])
+# def createuser():
+# 
+#     # Check if form submitted
+#     form = CreateUserForm(request.form)
+#
+#     if form.validate_on_submit():
+#         password = generate_password_hash(form.password.data)
+#         user = User('dectala', form.email.data, password)
+#         user.active = 0
+#         user.role = 0
+#         db.session.add(user)
+#         db.session.commit()
+#
+#         return "User Added"
+#
+#     return render_template("auth/signup.html", form=form)
--- spelllist/mod_auth/models.py
+++ spelllist/mod_auth/models.py
@@ -15,13 +15,30 @@ class User(Base):
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(128), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
-    active = db.Column(db.SmallInteger, nullable=False)
+    active = db.Column(db.Boolean, default=True)
     role = db.Column(db.SmallInteger, nullable=False)
+    authenticated = db.Column(db.Boolean, default=False)
+
 
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
         self.password = password
 
+
 def __repr__(self):
     return '<User %r>' % self.username
+
+
+def is_active(self):
+    # True, as all users are active.
+    return True
+
+
+def get_id(self):
+    return self.email
+
+
+def is_authenticated(self):
+    return self.authenticated
+
