923645b09f6825116fb8a9d310a82674b1a5945a
dectala <jemcgrew@gmail.com>
2016-04-18 15:50:31 -0400

    Fixed the error with signup, the models.py had a bad )

--- spelllist/mod_auth/forms.py
+++ spelllist/mod_auth/forms.py
@@ -10,4 +10,4 @@ class LoginForm(Form):
 class CreateUserForm(Form):
     email = StringField('Email Address', [Email(),DataRequired(message='Please enter your email!')])
     password = PasswordField('Password', [DataRequired(message='You need a Password!')])
-    username = StringField('UserName'), [DataRequired(message='You need a username')]
+    username = StringField('UserName', [DataRequired(message='You need a username')])
