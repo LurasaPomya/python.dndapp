910677c5fc11db82a2c9ca18574164b40b41174a
James <jemcgrew@gmail.com>
2016-06-11 22:26:48 -0400

    Models were missing Auto Increment for id field. This was causing problems.

--- dndapp/mod_auth/models.py
+++ dndapp/mod_auth/models.py
@@ -6,7 +6,7 @@ from flask_login import UserMixin
 class Base(db.Model):
     __abstract__ = True
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
--- dndapp/mod_spelllist/models.py
+++ dndapp/mod_spelllist/models.py
@@ -5,7 +5,7 @@ from dndapp import db
 class Base(db.Model):
     __abstract__ = True
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
 
 # Spell Model
