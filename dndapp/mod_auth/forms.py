66ee716918d9324494aa3c5679b00764993a0000
dectala <dectala@localhost.localdomain>
2017-01-12 00:33:00 -0800

    Updated from Dev Box. Changing alot of line endings. I think I'm done with Windows Python Dev.

--- dndapp/mod_auth/forms.py
+++ dndapp/mod_auth/forms.py
@@ -1,23 +1,23 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, IntegerField
 from wtforms.validators import DataRequired, Email
 
 
 # Login form
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     username = StringField('Username', [DataRequired(message='You need an Username!')])
     password = PasswordField('Password', [DataRequired(message='You need a Password!')])
 
 
 # Create user form
-class CreateUserForm(Form):
+class CreateUserForm(FlaskForm):
     email = StringField('Email Address', [Email(),DataRequired(message='Please enter your email!')])
     password = PasswordField('Password', [DataRequired(message='You need a Password!')])
     username = StringField('UserName', [DataRequired(message='You need a username')])
 
 
 # Change user form
-class ChangeUserPassword(Form):
+class ChangeUserPassword(FlaskForm):
     current_password = PasswordField('Current Password', [DataRequired(message='You must enter your current password!')])
     new_password = PasswordField('New Password', [DataRequired(message='Enter new password!')])
     new_password_repeat = PasswordField('New Password Again', [DataRequired(message='Enter new password Twice!')])
--- dndapp/mod_tasks/forms.py
+++ dndapp/mod_tasks/forms.py
@@ -1,10 +1,10 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 # Form to create task
-class CreateTaskForm(Form):
+class CreateTaskForm(FlaskForm):
     title = StringField('Task Title', [DataRequired(message='Please enter a Task Title!')])
     description = TextAreaField('Description', [DataRequired(message='Please enter a Description!')])
     priority = StringField('Priority', [DataRequired(message='Please Enter a Priority!')])
